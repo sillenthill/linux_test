bcbbfc15f9a488fed60423f1d02572ce0c729364
sillenthill <1048043980@qq.com>
2025-09-29 13:41:13 +0800

    add hello.c

---
+++ hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    
+    int a = 5;
+    int b = 3;
+    int sum = a + b;
+    
+    printf("The sum of %d and %d is: %d\n", a, b, sum);
+    
+    return 0;
+}
